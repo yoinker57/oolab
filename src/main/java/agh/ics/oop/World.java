e476a49ef302c0dec9d567e7e921813c0ea6146d
Radek Niżnik <radekniznik57@gmail.com>
2022-10-06 11:59:34 +0200

    commit 1

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,4 +1,26 @@
-package agh.ics.oop;
+¬package agh.ics.oop;
 
 public class World {
+    public World() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("system wystartowaĹ‚");
+        String[] Directions = new String[]{"FORWARD", "BACKWARD", "RIGHT", "LEFT"};
+        run(Directions);
+        System.out.println("system zakoĹ„czyĹ‚ dziaĹ‚anie");
+    }
+
+    public static void run(String[] args) {
+        System.out.println("zwierzak idzie do przodu");
+        String[] var1 = args;
+        int var2 = args.length;
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            String element = var1[var3];
+            System.out.println(element);
+        }
+
+    }
 }
+
